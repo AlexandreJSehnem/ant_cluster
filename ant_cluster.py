53b38f7cad0ac5e6ac87bdfb6858f77daa4e1964
AlexandreJSehnem <alexandre.jsehnem@gmail.com>
2022-05-02 02:09:23 -0300

    Added pickup, drop and move to the ants actions, currently only working with 1 agent

--- ant_cluster.py
+++ ant_cluster.py
@@ -4,8 +4,11 @@ import threading
 SIGHT_RANGE = 1
 WIDTH = 50
 HEIGHT = 50
-CORPSES = 1000
-AGENTS = 10
+CORPSES = 1500
+AGENTS = 1 # currently only works with one agent
+STEPS = 1000000
+
+lock_move = threading.Lock()
 
 class ant(threading.Thread):
 
@@ -14,14 +17,109 @@ class ant(threading.Thread):
         self.pos_x = x
         self.pos_y = y
         self.sight = range
-        self.field: map = map_class
+        self.map_info: map = map_class
+        self.steps = STEPS
         self.holding = False
     
     def run(self):
         self.brain()
 
     def brain(self):
-        pass
+        # main step loop
+        for i in range(self.steps):
+            # decides if it should pickup or drop
+            if self.holding:
+                if self.should_drop():
+                    self.map_info.field[self.pos_x][self.pos_y] = 1
+                    self.holding = False
+            else:
+                if self.should_pickup():
+                    self.map_info.field[self.pos_x][self.pos_y] = 0
+                    self.holding = True
+            self.move()
+
+    def should_drop(self):
+        if self.map_info.field[self.pos_x][self.pos_y] == 0:
+            area = ((self.sight*2) + 1)*((self.sight*2) + 1)
+            chance = randrange(0,area)
+            if chance <= self.amount_dead(): return True
+            else: return False
+
+    def should_pickup(self):
+        if self.map_info.field[self.pos_x][self.pos_y] == 1:
+            area = ((self.sight*2) + 1)*((self.sight*2) + 1)
+            chance = randrange(1,area+1)
+            if chance > self.amount_dead(): return True
+            else: return False
+
+
+    def amount_dead(self):
+        amount_dead = 0
+        for i in range(self.sight*2 + 1):
+            temp_i = 0
+            if i < self.sight+1: temp_i = self.pos_x - i
+            elif i > self.sight+1: temp_i = self.pos_x + i - self.sight + 1
+            else: temp_i = self.pos_x
+            for j in range(self.sight*2 + 1):
+                temp_j = 0
+                if j < self.sight+1: temp_j = self.pos_y - j
+                elif j > self.sight+1: temp_j = self.pos_y + j - self.sight + 1
+                else: temp_j = self.pos_y
+                # sums the designated position
+                amount_dead = amount_dead + self.map_info.field[temp_i][temp_j]
+        return amount_dead
+
+    def move(self):
+        moved = False
+        while not moved:
+            x = randrange(0,4) # 0 = west, 1 = south, 2 = east, 3 = north
+            lock_move.acquire()
+            if x == 0:
+                temp_x = self.pos_x
+                if (self.pos_x + 1) == self.map_info.width: temp_x = 0
+                else: temp_x += 1
+                if self.map_info.ant_location[temp_x][self.pos_y]:
+                    continue
+                else:
+                    self.map_info.ant_location[temp_x][self.pos_y] = True
+                    self.map_info.ant_location[self.pos_x][self.pos_y] = False
+                    self.pos_x = temp_x
+                    moved = True
+            elif x == 1:
+                temp_y = self.pos_y
+                if (self.pos_y + 1) == self.map_info.height: temp_y = 0
+                else: temp_y += 1
+                if self.map_info.ant_location[self.pos_x][temp_y]:
+                    continue
+                else:
+                    self.map_info.ant_location[self.pos_x][temp_y] = True
+                    self.map_info.ant_location[self.pos_x][self.pos_y] = False
+                    self.pos_y = temp_y
+                    moved = True
+            elif x == 2:
+                temp_x = self.pos_x
+                if (self.pos_x - 1) < 0: temp_x = self.map_info.width - 1
+                else: temp_x = temp_x - 1
+                if self.map_info.ant_location[temp_x][self.pos_y]:
+                    continue
+                else:
+                    self.map_info.ant_location[temp_x][self.pos_y] = True
+                    self.map_info.ant_location[self.pos_x][self.pos_y] = False
+                    self.pos_x = temp_x
+                    moved = True
+            elif x == 3:
+                temp_y = self.pos_y
+                if (self.pos_y - 1) < 0: temp_y = self.map_info.height - 1
+                else: temp_y = temp_y - 1
+                if self.map_info.ant_location[self.pos_x][temp_y]:
+                    continue
+                else:
+                    self.map_info.ant_location[self.pos_x][temp_y] = True
+                    self.map_info.ant_location[self.pos_x][self.pos_y] = False
+                    self.pos_y = temp_y
+                    moved = True
+            lock_move.release()
+            # print(f"destination: {x} new x: {self.pos_x} new y:{self.pos_y}")
 
 class map():
 
@@ -33,6 +131,7 @@ class map():
         self.create_field(corpses)
         pass
 
+    # generates dead ants and places them randomly
     def create_field(self, corpses):
         i = 0
         for i in range(self.width):
@@ -43,7 +142,6 @@ class map():
                 ants.append(False)
             self.field.append(line)
             self.ant_location.append(ants)
-        count = 0
         for i in range(corpses):
             x = randrange(0, self.width)
             y = randrange(0, self.height)
@@ -51,10 +149,8 @@ class map():
                 x = randrange(0, self.width)
                 y = randrange(0, self.height)
             self.field[x][y] = 1
-            count+=1
-        self.print_field()
-        print(count)
 
+    # prints the current field state
     def print_field(self):
         for i in range(self.width):
             printable = ""
@@ -68,6 +164,7 @@ def read_file():
         continue
     pass
 
+# starts map and ant threads
 mapper = map(WIDTH, HEIGHT, CORPSES)
 ant_list = []
 for i in range(AGENTS):
@@ -84,4 +181,10 @@ for i in range(AGENTS):
     ant_list.append(thread)
     thread.start()
 
-print("bruh")
+# waits all ants to finish moving
+for thread in ant_list:
+    print("ants")
+    thread.join()
+
+# prints final result
+mapper.print_field()
