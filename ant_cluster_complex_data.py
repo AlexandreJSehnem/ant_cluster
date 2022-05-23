59dea84463f7452f6d2031c7aaf89957926ce873
AlexandreJSehnem <alexandre.jsehnem@gmail.com>
2022-05-23 14:31:50 -0300

    Update ant_cluster_complex_data.py
    a bunch of changes and fixes made to the math behin the decision making

--- ant_cluster_complex_data.py
+++ ant_cluster_complex_data.py
@@ -1,16 +1,18 @@
-from random import randrange, shuffle
+from random import shuffle, random, randrange
 from math import sqrt
-import os
+# import os
 
 SIGHT_RANGE = 1
-WIDTH = 20
-HEIGHT = 20
+WIDTH = 50
+HEIGHT = 100
 CORPSES = 1000
-AGENTS = 1 # currently only works with one agent
-STEPS = 1000000
-ALPHA = 10
-K1 = 0.5
-K2 = 0.5
+AGENTS = 1  # currently only works with one agent
+STEPS = 10000000
+ALPHA = 4
+K1 = 0.05
+K2 = 0.01
+total_picks = 0
+total_drops = 0
 
 
 class ant():
@@ -34,10 +36,10 @@ class ant():
                 else:
                     self.map_info.field[self.pos_x][self.pos_y] = self.being_hold
                     self.being_hold = aux
-        else:
+        elif self.map_info.field[self.pos_x][self.pos_y][2] != 0:
             if self.should_pickup():
                 self.being_hold = self.map_info.field[self.pos_x][self.pos_y]
-                self.map_info.field[self.pos_x][self.pos_y] = [0,0,0]
+                self.map_info.field[self.pos_x][self.pos_y] = ["0", "0", 0]
                 self.holding = True
         self.move()
 
@@ -45,54 +47,80 @@ class ant():
         area = ((self.sight*2) + 1)*((self.sight*2) + 1)
         ed = (self.euclidian_distance())/(area*area)
         chance = (ed/(K2+ed))*(ed/(K2+ed))
-        chance = chance * 100
-        n = randrange(0,101)
-        if n > chance:
+        # print("Drops man:"+str(chance)+" "+str(ed))
+        n = random()
+        global total_drops
+        if n < chance:
+            total_drops += 1
             # print("eo")
             return True
-        else: return False
+        else:
+            return False
 
     def should_pickup(self):
         area = ((self.sight*2) + 1)*((self.sight*2) + 1)
-        ed = (self.euclidian_distance())/(area*area)
+        pre_ed = self.euclidian_distance()
+        # if pre_ed == 0: pre_ed = 1
+        ed = (pre_ed)/(area*area)
         chance = (K1/(K1+ed))*(K1/(K1+ed))
-        chance = chance * 100
-        n = randrange(0,101)
+        # print(str(pre_ed)+"  "+str(ed)+"  "+str(chance))
+        n = random()
+        global total_picks
         if n > chance:
+            total_picks += 1
             return True
-        else: return False
+        else:
+            return False
 
     def euclidian_distance(self):
         distance = 0
+        to_compare = []
+        count_to_divide = 0
+        if self.holding:
+            to_compare = self.being_hold
+        else:
+            to_compare = self.map_info.field[self.pos_x][self.pos_y]
         for i in range(self.sight*2 + 1):
             temp_i = 0
-            if i < self.sight+1: temp_i = self.pos_x - i
-            elif i > self.sight+1: temp_i = self.pos_x + i - self.sight + 1
-            else: temp_i = self.pos_x
+            if i < self.sight + 1:
+                temp_i = self.pos_x - i
+            elif i > self.sight + 1:
+                temp_i = self.pos_x + i - self.sight + 1
+            else:
+                temp_i = self.pos_x
             for j in range(self.sight*2 + 1):
                 temp_j = 0
-                if j < self.sight+1: temp_j = self.pos_y - j
-                elif j > self.sight+1: temp_j = self.pos_y + j - self.sight + 1
-                else: temp_j = self.pos_y
+                if j < self.sight + 1:
+                    temp_j = self.pos_y - j
+                elif j > self.sight + 1:
+                    temp_j = self.pos_y + j - self.sight + 1
+                else:
+                    temp_j = self.pos_y
                 # sums the designated position
                 if temp_i >= WIDTH:
                     temp_i = temp_i - WIDTH
                 if temp_j >= HEIGHT:
                     temp_j = temp_j - HEIGHT
-                distance = distance + (1 - sqrt(
-                    (float(self.map_info.field[self.pos_x][self.pos_y][0]) - float(self.map_info.field[temp_i][temp_j][0]))*(float(self.map_info.field[self.pos_x][self.pos_y][0]) - float(self.map_info.field[temp_i][temp_j][0])) +
-                    (float(self.map_info.field[self.pos_x][self.pos_y][1]) - float(self.map_info.field[temp_i][temp_j][1]))*(float(self.map_info.field[self.pos_x][self.pos_y][1]) - float(self.map_info.field[temp_i][temp_j][1]))
-                    )/ALPHA)
+                if self.map_info.field[temp_i][temp_j][2] != 0:
+                    count_to_divide += 1
+                    distance = distance + (1 - sqrt(
+                        (float(to_compare[0]) - float(self.map_info.field[temp_i][temp_j][0]))*(float(to_compare[0]) - float(self.map_info.field[temp_i][temp_j][0])) +
+                        (float(to_compare[1]) - float(self.map_info.field[temp_i][temp_j][1]))*(float(to_compare[1]) - float(self.map_info.field[temp_i][temp_j][1]))
+                        )/ALPHA)
+        if distance < 0:
+            return 0
         return distance
 
     def move(self):
         moved = False
         while not moved:
-            x = randrange(0,4) # 0 = west, 1 = south, 2 = east, 3 = north
+            x = randrange(0, 4)  # 0 = west, 1 = south, 2 = east, 3 = north
             if x == 0:
                 temp_x = self.pos_x
-                if (self.pos_x + 1) == self.map_info.width: temp_x = 0
-                else: temp_x += 1
+                if (self.pos_x + 1) == self.map_info.width:
+                    temp_x = 0
+                else:
+                    temp_x += 1
                 if self.map_info.ant_location[temp_x][self.pos_y]:
                     continue
                 else:
@@ -102,8 +130,10 @@ class ant():
                     moved = True
             elif x == 1:
                 temp_y = self.pos_y
-                if (self.pos_y + 1) == self.map_info.height: temp_y = 0
-                else: temp_y += 1
+                if (self.pos_y + 1) == self.map_info.height:
+                    temp_y = 0
+                else:
+                    temp_y += 1
                 if self.map_info.ant_location[self.pos_x][temp_y]:
                     continue
                 else:
@@ -113,8 +143,10 @@ class ant():
                     moved = True
             elif x == 2:
                 temp_x = self.pos_x
-                if (self.pos_x - 1) < 0: temp_x = self.map_info.width - 1
-                else: temp_x = temp_x - 1
+                if (self.pos_x - 1) < 0:
+                    temp_x = self.map_info.width - 1
+                else:
+                    temp_x = temp_x - 1
                 if self.map_info.ant_location[temp_x][self.pos_y]:
                     continue
                 else:
@@ -124,8 +156,10 @@ class ant():
                     moved = True
             elif x == 3:
                 temp_y = self.pos_y
-                if (self.pos_y - 1) < 0: temp_y = self.map_info.height - 1
-                else: temp_y = temp_y - 1
+                if (self.pos_y - 1) < 0:
+                    temp_y = self.map_info.height - 1
+                else:
+                    temp_y = temp_y - 1
                 if self.map_info.ant_location[self.pos_x][temp_y]:
                     continue
                 else:
@@ -134,6 +168,7 @@ class ant():
                     self.pos_y = temp_y
                     moved = True
             # print(f"destination: {x} new x: {self.pos_x} new y:{self.pos_y}")
+
 
 class map():
 
@@ -168,8 +203,12 @@ class map():
         for i in range(self.width):
             printable = ""
             for j in range(self.height):
-                printable += str(self.field[i][j][2])
+                if self.field[i][j][2] == 0:
+                    printable += " "
+                else:
+                    printable += str(self.field[i][j][2])
             print(printable)
+
 
 def read_file():
     file = open("input.txt", "r")
@@ -180,8 +219,12 @@ def read_file():
     file.close()
     return data_list
 
+
 data_list = read_file()
 print(data_list.__len__())
+size = data_list.__len__()
+for i in range((WIDTH*HEIGHT)-size):
+    data_list.append(["0", "0", 0])
 # starts map
 mapper = map(WIDTH, HEIGHT, data_list)
 
@@ -218,5 +261,7 @@ while True:
         step_count += 1
 
 # prints final result
-print("")
+print("-----------------------------")
 mapper.print_field()
+print(total_picks)
+print(total_drops)
